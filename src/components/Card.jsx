d2d385f852c87a7c25653ee248825c303ebfd264
Lakshitha Kaveen <115539769+lakshithaKaveen@users.noreply.github.com>
2024-05-12 23:03:10 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -127,7 +127,7 @@ const Card = ({name, category, price, buttonText, courseID}) => {
         </div>
 
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-green-600">{price}</span>
+          <span className="text-3xl font-bold text-green-600">$ {price}</span>
           <button
             onClick={enrollCourse}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
